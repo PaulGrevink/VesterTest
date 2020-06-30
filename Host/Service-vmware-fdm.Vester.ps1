efa759f8804c8ab1dcc02adcba20ad2998ddb963
Paul Grevink <paul.grevink@gmail.com>
2020-06-30 20:32:55 +0200

    Add files via upload

---
+++ Host/Service-vmware-fdm.Vester.ps1
@@ -0,0 +1,44 @@
+﻿# Test file for the Vester module - https://github.com/WahlNetwork/Vester
+# Called via Invoke-Pester VesterTemplate.Tests.ps1
+
+# Test title, e.g. 'DNS Servers'
+$Title = 'vSphere High Availability Agent Service State'
+
+# Test description: How New-VesterConfig explains this value to the user
+$Description = 'On/off switch for vSphere High Availability Agent'
+# Must be TRUE, but FALSE during Maintenanced Mode.
+
+# The config entry stating the desired values
+$Desired = $cfg.host.service_vmwarefdm_running
+
+# The test value's data type, to help with conversion: bool/string/int
+$Type = 'bool'
+
+# The command(s) to pull the actual value for comparison
+# $Object will scope to the folder this test is in (Cluster, Host, etc.)
+[ScriptBlock]$Actual = {
+    ($Object | Get-VMHostService | Where-Object -FilterScript {
+        $_.Key -eq 'vmware-fdm'
+    }).Running
+}
+
+# The command(s) to match the environment to the config
+# Use $Object to help filter, and $Desired to set the correct value
+[ScriptBlock]$Fix = {
+    if ($Desired -eq $true) 
+    {
+        Start-VMHostService -HostService ($Object |
+            Get-VMHostService |
+            Where-Object -FilterScript {
+                $_.Key -eq 'vmware-fdm'
+        }) -ErrorAction Stop -Confirm:$false
+    }
+    if ($Desired -eq $false) 
+    {
+        Stop-VMHostService -HostService ($Object |
+            Get-VMHostService |
+            Where-Object -FilterScript {
+                $_.Key -eq 'vmware-fdm'
+        }) -ErrorAction Stop -Confirm:$false
+    }
+}
